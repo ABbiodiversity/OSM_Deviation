70b2ccd7f5b4437de82c13aa8f4b1cefc0015b8c
ecknight <ellyknight@gmail.com>
2025-03-03 13:42:05 -0700

    interim solution until I get the new data processed

--- 02.WrangleData.R
+++ 02.WrangleData.R
@@ -25,8 +25,8 @@ load(file.path(rootin,"Data", "Stratified.Rdata"))
 lu <- read_sf(file.path(rootout, "GIS", "OSR_LU_fxd", "OSR_LUs_fxd.shp"))
 
 ## 1.5 Import BADR treatment ----
-badr <- read_sf(file.path(rootout, "GIS", "year_2021_treat.shp")) |> 
-  st_make_valid()
+# badr <- read_sf(file.path(rootout, "GIS", "year_2021_treat.shp")) |> 
+#   st_make_valid()
 
 ## 1.6 Import the newly downloaded data ----
 load(file.path(rootout, "Deviation From Expected", "Data", "NewWTData.Rdata"))
@@ -74,87 +74,93 @@ bird_train <- bird |>
 off_train <- off |> 
   dplyr::filter(surveyid %in% covs_train$surveyid)
 
-#4. Add new data to test dataset----
-
-## 4.1 Add survey ids ----
-new_wide <- dat_wide |> 
-  mutate(surveyid = row_number() + max(covs$surveyid))
-
-new_off <- dat_off  |> 
-  mutate(surveyid = row_number() + max(covs$surveyid))
-  
-## 4.1 Filter to years of interest and AOI ----
-new_aoi <- new_wide |> 
-  dplyr::filter(!is.na(longitude), !is.na(latitude),
-                aru_task_status=="Transcribed",
-                task_method=="1SPT") |> 
-  st_as_sf(coords = c("longitude", "latitude"), crs=4326) |> 
-  st_transform(crs=3400) |> 
-  st_coordinates() |> 
-  data.frame() |> 
-  rename(Easting = X, Northing = Y) |> 
-  cbind(new_wide  |> 
-          dplyr::filter(!is.na(longitude), !is.na(latitude),
-                        aru_task_status=="Transcribed",
-                        task_method=="1SPT")) |> 
-  mutate(recording_date_time = ymd_hms(recording_date_time),
-         year = year(recording_date_time)) |> 
-  rename(method = task_method) |> 
-  dplyr::filter(year >= 2020) |> 
-  st_as_sf(coords=c("Easting", "Northing"), crs=3400, remove=FALSE) |> 
-  st_transform(raster::crs(aoi)) |> 
-  st_intersection(aoi)
-
-## 4.2 Separate into bird and covs objects ----
-covs_new <- new_aoi |> 
-  st_drop_geometry() |> 
-  dplyr::select(organization, project_id, surveyid, method, Easting, Northing, year)
-
-bird_new <- new_aoi |> 
-  st_drop_geometry() |> 
-  dplyr::select(surveyid, ALFL:YRWA)
-
-## 4.3 Identify duplicates ----
-loc_new <- covs_new |> 
-  dplyr::select(Easting, Northing, year) |> 
-  unique()
-
-loc_test <- covs_test |> 
-  dplyr::select(Easting, Northing, year) |> 
-  unique()
-
-duplicates <- inner_join(loc_new, loc_test)
-
-## 4.4. Put together ----
-surveys_test <- covs_test |> 
-  dplyr::select(all_of(colnames(covs_new))) |> 
-  anti_join(duplicates) |> 
-  rbind(covs_new) |> 
-  arrange(surveyid)
-
-## 4.5 Filter the bird and offset objects ----
-# Also filter to indicator species
-spp <- sort(c("CONI", "LEFL", "CMWA", "HETH", "OVEN", "PIWO", "BTNW", "NOWA", "CAJA", "RUGR", "YBSA", "DEJU", "BHCO", "CAWA", "OSFL"))
-
 bird_test <- bird |> 
-  dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
-  dplyr::select(all_of(c("surveyid", spp))) |> 
-  rbind(bird_new  |> 
-          dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
-          dplyr::select(all_of(c("surveyid", spp)))) |> 
-  arrange(surveyid)
+  dplyr::filter(surveyid %in% covs_test$surveyid)
 
-off_test <- off |> 
-  dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
-  dplyr::select(all_of(c("surveyid", spp))) |> 
-  rbind(new_off  |> 
-          dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
-          dplyr::select(all_of(c("surveyid", spp)))) |> 
-  arrange(surveyid)
+off_test <- bird |> 
+  dplyr::filter(surveyid %in% covs_test$surveyid)
+# 
+# #4. Add new data to test dataset----
+# 
+# ## 4.1 Add survey ids ----
+# new_wide <- dat_wide |> 
+#   mutate(surveyid = row_number() + max(covs$surveyid))
+# 
+# new_off <- dat_off  |> 
+#   mutate(surveyid = row_number() + max(covs$surveyid))
+#   
+# ## 4.1 Filter to years of interest and AOI ----
+# new_aoi <- new_wide |> 
+#   dplyr::filter(!is.na(longitude), !is.na(latitude),
+#                 aru_task_status=="Transcribed",
+#                 task_method=="1SPT") |> 
+#   st_as_sf(coords = c("longitude", "latitude"), crs=4326) |> 
+#   st_transform(crs=3400) |> 
+#   st_coordinates() |> 
+#   data.frame() |> 
+#   rename(Easting = X, Northing = Y) |> 
+#   cbind(new_wide  |> 
+#           dplyr::filter(!is.na(longitude), !is.na(latitude),
+#                         aru_task_status=="Transcribed",
+#                         task_method=="1SPT")) |> 
+#   mutate(recording_date_time = ymd_hms(recording_date_time),
+#          year = year(recording_date_time)) |> 
+#   rename(method = task_method) |> 
+#   dplyr::filter(year >= 2020) |> 
+#   st_as_sf(coords=c("Easting", "Northing"), crs=3400, remove=FALSE) |> 
+#   st_transform(raster::crs(aoi)) |> 
+#   st_intersection(aoi)
+# 
+# ## 4.2 Separate into bird and covs objects ----
+# covs_new <- new_aoi |> 
+#   st_drop_geometry() |> 
+#   dplyr::select(organization, project_id, surveyid, method, Easting, Northing, year)
+# 
+# bird_new <- new_aoi |> 
+#   st_drop_geometry() |> 
+#   dplyr::select(surveyid, ALFL:YRWA)
+# 
+# ## 4.3 Identify duplicates ----
+# loc_new <- covs_new |> 
+#   dplyr::select(Easting, Northing, year) |> 
+#   unique()
+# 
+# loc_test <- covs_test |> 
+#   dplyr::select(Easting, Northing, year) |> 
+#   unique()
+# 
+# duplicates <- inner_join(loc_new, loc_test)
+# 
+# ## 4.4. Put together ----
+# surveys_test <- covs_test |> 
+#   dplyr::select(all_of(colnames(covs_new))) |> 
+#   anti_join(duplicates) |> 
+#   rbind(covs_new) |> 
+#   arrange(surveyid)
+# 
+# ## 4.5 Filter the bird and offset objects ----
+# # Also filter to indicator species
+# spp <- sort(c("CONI", "LEFL", "CMWA", "HETH", "OVEN", "PIWO", "BTNW", "NOWA", "CAJA", "RUGR", "YBSA", "DEJU", "BHCO", "CAWA", "OSFL"))
+# 
+# bird_test <- bird |> 
+#   dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
+#   dplyr::select(all_of(c("surveyid", spp))) |> 
+#   rbind(bird_new  |> 
+#           dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
+#           dplyr::select(all_of(c("surveyid", spp)))) |> 
+#   arrange(surveyid)
+# 
+# off_test <- off |> 
+#   dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
+#   dplyr::select(all_of(c("surveyid", spp))) |> 
+#   rbind(new_off  |> 
+#           dplyr::filter(surveyid %in% surveys_test$surveyid) |> 
+#           dplyr::select(all_of(c("surveyid", spp)))) |> 
+#   arrange(surveyid)
 
-#4. BADR attribution ----
+#5. BADR attribution ----
 
-## 4.1 Make an sf with 150 m buffer -----
+## 5.1 Make an sf with 150 m buffer -----
 # get the area and transform to the badr object crs
 covs_buff <- surveys_test |> 
   st_as_sf(coords = c("Easting", "Northing"), crs=3400, remove=FALSE) |> 
@@ -162,10 +168,10 @@ covs_buff <- surveys_test |>
   mutate(area = round(as.numeric(st_area(geometry)), 1)) |> 
   st_transform(raster::crs(badr))
 
-## 4.2 Get the lu attributes -----
+## 5.2 Get the lu attributes -----
 covs_lu <- st_intersection(covs_buff, lu)
 
-## 4.3 Get the BADR treatment proportions ----
+## 5.3 Get the BADR treatment proportions ----
 set.seed(1234)
 covs_test <- covs_lu |> 
 # sample_n(10) |> 
@@ -174,7 +180,7 @@ covs_test <- covs_lu |>
   st_drop_geometry() |> 
   pivot_wider(names_from=Treatment, values_from=tmntarea, values_fn=sum, values_fill=0)
   
-#5. Save ----
+#6. Save ----
 save(covs_train, bird_train, off_train, boot, file=file.path(rootout, "Deviation From Expected", "Data", "Train.Rdata"))
 
-save(covs_test, bird_test, file=file.path(rootout, "Deviation From Expected", "Data", "Test.Rdata"))
+save(covs_test, bird_test, off_test, file=file.path(rootout, "Deviation From Expected", "Data", "Test.Rdata"))
