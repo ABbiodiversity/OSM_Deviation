e76f094d3737b5d595508eb6da5bfbd71ca2cb83
ecknight <ellyknight@gmail.com>
2025-03-23 18:59:50 -0600

    update without BRT

--- 11.SummaryStats.R
+++ 11.SummaryStats.R
@@ -116,129 +116,52 @@ coef <- read.csv(file.path(root, "Results", "Coefficients.csv"))
 
 # 4. Deviation -----
 
-## 4.1 Get list of completed predictions ----
-done <- data.frame(file = list.files(file.path(root, "Results", "Predictions"), pattern="*.csv", recursive=TRUE),
-                   path = list.files(file.path(root, "Results", "Predictions"), pattern="*.csv", recursive=TRUE, full.names = TRUE)) |>
-  separate(file, into=c("prediction", "spf", "species", "bootstrap", "filetype")) |>
-  dplyr::select(-filetype, -spf, -prediction)
+## 4.1 Read in predictions ----
+pred <- read.csv(file.path(root, "Results", "Predictions.csv"))
 
-## 4.2 Read in predictions ----
-pred <- map_dfr(read.csv, .x=done$path)
-
-## 4.3 Save this for figures ----
-write.csv(pred, file.path(root, "Results", "Predictions.csv"), row.names = FALSE)
-
-## 4.4 Summarize ----
+## 4.2 Summarize ----
 pred.sum <- pred |> 
   dplyr::filter(species!="CONI") |> 
-  group_by(species, boot) |> 
+  group_by(dataset, species, boot) |> 
   summarize(diff_mn = mean(residual),
             pred_mn = mean(prediction)) |> 
   ungroup() |> 
   mutate(perc_mn = diff_mn/pred_mn*100) |> 
-  group_by(species) |> 
+  group_by(species, dataset) |> 
   summarize(mn = mean(perc_mn),
             se = sd(perc_mn)/5) |> 
   ungroup() |> 
   mutate(lwr = mn - 1.96*se,
          upr = mn + 1.96*se)
 
-## 4.6 Deviation ----
+## 4.3 Deviation across species by dataset ----
 pred.sum |> 
-  dplyr::filter(mn < 0) |> 
-  summary()
+  group_by(dataset) |> 
+  summarize(mean = mean(mn),
+            sd = sd(mn))
 
-pred.sum |> 
-  mutate(lwr = mn - q.96*se)
+## 4.4 Read in t-tests ----
+ts <- read.csv(file.path(root, "Deviation From Expected", "Results", "PredictionComparison.csv")) |> 
+  dplyr::filter(measure=="Percent") 
 
-# 5. Deviation explained ----
+## 4.5 Summarize ----
+ts |> 
+  mutate(sig = ifelse(p < 0.05, 1, 0)) |> 
+  summarize(sigs = sum(sig))
 
-## 5.1 Get list of model performance files ----
-perf.files <- data.frame(path = list.files(file.path(root, "Results", "BRTs", "ModelPerformance"), pattern="*.csv", full.names = TRUE))
+ts |> 
+  mutate(dir = ifelse(diff < 0, "neg", "pos")) |> 
+  group_by(dir) |> 
+  summarize(count = n())
 
-## 5.2 Read them in ----
-perf <- map_dfr(read.csv, .x=perf.files$path)
+## 4.6 Tidy for appendix 2
+app2 <- ts |> 
+  mutate(t = round(t, 2),
+         p = round(p, 3),
+         diff = round(diff, 2),
+         low = round(low, 2),
+         high = round(high, 2)) |> 
+  mutate(difference = paste0(diff, " (", low, ", ", high, " 95% CI)")) |> 
+  dplyr::select(species, t, p, difference) 
 
-## 5.3 Calculate deviance explained ----
-perf.sum <- perf |> 
-  mutate(dev = (deviance.mean - resid)/deviance.mean) |> 
-  group_by(species) |> 
-  summarize(dev.mn = mean(dev),
-            dev.se = sd(dev)/(sqrt(25))) |> 
-  ungroup() |> 
-  dplyr::filter(species!="CONI")
-mean(perf.sum$dev.mn)
-sd(perf.sum$dev.mn)
-
-# 6. Landscape scale effects ----
-
-## 6.1 Get the variables importance ----
-var <- read.csv(file.path(root, "Results", "BRTVariableImportance.csv"))
-
-## 6.2 Classify vars ----
-names <- data.frame(var = c("badr_highwells", "badr_linear", "badr_lowwells", "badr_mine", "badr_minebuffer", "badr_roads", "cei", "propallwel","proppipe", "propmine", "proproad", "propseismi", "year"),
-                    scale = c(rep("JEM", 6), rep("LU", 6), "time"),
-                    name = c("High impact wellsites", "Linear features", "Low impact wellsites", "Mine/plant", "Mine/plant buffer", "Roads",
-                             "Cumulative footprint index", "Wellsites",  "Pipelines", "Mine/plant", "Road", "Seismic lines",
-                             "Year"))
-
-## 6.3 Wrangle to % deviance ----
-var.perf <- var |> 
-  rename(bootstrap = boot) |> 
-  left_join(perf |> 
-              mutate(dev = (deviance.mean - resid)/deviance.mean) |> 
-              dplyr::select(species, bootstrap, dev),
-            multiple="all") |> 
-  dplyr::select(-X) |> 
-  mutate(var.dev = dev*rel.inf/100) |> 
-  group_by(species, var) |> 
-  summarize(dev.mn = mean(var.dev),
-            dev.se = sd(var.dev)/5) |> 
-  ungroup() |> 
-  left_join(names)
-
-## 6.4 Just landscape scale ----
-var.lu <- var.perf |> 
-  dplyr::filter(scale=="LU")
-
-summary(var.lu)
-
-## 6.5 Let's try looking at one ----
-pred <- read.csv(file.path(root, "Results", "Predictions.csv"))
-load(file.path(root, "Data", "Test.Rdata"))
-covs_use <- covs_badr |> 
-  rename(badr_highwells = `High Activity Insitu Well Pads`,
-         badr_roads = Roads,
-         badr_mine = `Plant/Mine`,
-         badr_linear = `Dense Linear Features`,
-         badr_lowwells = `Low Activity Well Pads`,
-         badr_minebuffer = `Plant/Mine Buffer`,
-         badr_reference = `Low Disturbance/Reference`) |> 
-  dplyr::select(surveyid, year, cei, propmine, proproad, propseismi, propallwel, proppipe, badr_highwells, badr_roads, badr_mine, badr_linear, badr_lowwells, badr_minebuffer)
-
-pred.eg <- pred |> 
-  dplyr::filter(species=="OVEN") |> 
-  left_join(covs_use) |> 
-  mutate(cei_c = case_when(cei < 0.4 ~ "low",
-                           cei > 0.8 ~ "high"),
-         badr_linear_c = case_when(badr_linear < 0.1 ~ "low",
-                                   badr_linear < 0.8 ~ "high"),
-         badr_lowwells_c = case_when(badr_lowwells < 0.1 ~ "low",
-                                     badr_lowwells > 0.9 ~ "high"),
-         badr_roads_c = case_when(badr_roads < 0.2 ~ "low",
-                                  badr_roads > 0.8 ~ "high"),
-         propmine_c = case_when(propmine > 0.02 ~ "high",
-                                propmine < 0.02 ~ "low"),
-         proproad_c = case_when(proproad < 0.001 ~ "low",
-                                proproad > 0.010 ~ "high"))
-
-ggplot(pred.eg) +
-  geom_point(aes(x=badr_roads, y=residual)) +
-  geom_smooth(aes(x=badr_roads, y=residual), method="lm")
-
-ggplot(pred.eg) +
-  geom_boxplot(aes(x=badr_roads_c, y=residual))
-
-t.test(residual ~ badr_roads_c, data=pred.eg)
-
-hist(pred.eg$residual)
+write.csv(app2, file.path(root, "Deviation From Expected", "Results", "Appendix2.csv"), row.names = FALSE)
